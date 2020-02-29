cf82d4f2a3666223424edc263aa5b1153c5c9c36
EntryPointKR <entrypointkr@gmail.com>
2020-02-29 18:38:55 +0900

    Make repository extensions configurable

--- src/main/kotlin/kr/entree/spigradle/kotlin/repository/RepositoryHandlers.kt
+++ src/main/kotlin/kr/entree/spigradle/kotlin/repository/RepositoryHandlers.kt
@@ -2,24 +2,29 @@ package kr.entree.spigradle.kotlin.repository
 
 import kr.entree.spigradle.project.Repositories
 import org.gradle.api.artifacts.dsl.RepositoryHandler
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository
 
 /**
  * Created by JunHyung Lim on 2020-02-28
  */
-internal fun RepositoryHandler.maven(url: String) = maven {
+internal inline fun RepositoryHandler.maven(
+        url: String,
+        crossinline configure: MavenArtifactRepository.() -> Unit = {}
+) = maven {
     it.setUrl(url)
+    configure(it)
 }
 
-fun RepositoryHandler.spigot() = maven(Repositories.SPIGOT)
+fun RepositoryHandler.spigot(configure: MavenArtifactRepository.() -> Unit = {}) = maven(Repositories.SPIGOT, configure)
 
-fun RepositoryHandler.bungeecord() = maven(Repositories.BUNGEECORD)
+fun RepositoryHandler.bungeecord(configure: MavenArtifactRepository.() -> Unit = {}) = maven(Repositories.BUNGEECORD, configure)
 
-fun RepositoryHandler.paper() = maven(Repositories.PAPER)
+fun RepositoryHandler.paper(configure: MavenArtifactRepository.() -> Unit = {}) = maven(Repositories.PAPER, configure)
 
-fun RepositoryHandler.protocolLib() = maven(Repositories.PROTOCOL_LIB)
+fun RepositoryHandler.protocolLib(configure: MavenArtifactRepository.() -> Unit = {}) = maven(Repositories.PROTOCOL_LIB, configure)
 
-fun RepositoryHandler.jitpack() = maven(Repositories.JITPACK)
+fun RepositoryHandler.jitpack(configure: MavenArtifactRepository.() -> Unit = {}) = maven(Repositories.JITPACK, configure)
 
-fun RepositoryHandler.vault() = jitpack()
+fun RepositoryHandler.vault(configure: MavenArtifactRepository.() -> Unit = {}) = jitpack(configure)
 
-fun RepositoryHandler.enginehub() = maven(Repositories.ENGINEHUB)
+fun RepositoryHandler.enginehub(configure: MavenArtifactRepository.() -> Unit = {}) = maven(Repositories.ENGINEHUB, configure)
