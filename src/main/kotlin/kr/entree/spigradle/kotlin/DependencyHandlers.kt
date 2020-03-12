af7569999cc61f56992c672c28dea244df56f984
Junhyung Im <entrypointkr@gmail.com>
2020-03-12 19:11:16 +0900

    Update 1.2.1: Add bungeecord() dependency, Bump default version to 1.15.2

--- src/main/kotlin/kr/entree/spigradle/kotlin/DependencyHandlers.kt
+++ src/main/kotlin/kr/entree/spigradle/kotlin/DependencyHandlers.kt
@@ -8,32 +8,34 @@ fun DependencyHandler.spigradle(version: String = SpigradleMeta.VERSION) = SPIGR
 
 fun DependencyHandler.spigotmc(artifactId: String, version: String) = "${SPIGOT.groupId}:$artifactId:$version"
 
-fun DependencyHandler.spigot(version: String = "") = SPIGOT.format(version)
+fun DependencyHandler.spigot(version: String = SPIGOT.defaultVersion) = SPIGOT.format(version)
 
-fun DependencyHandler.spigotAll(version: String = "") = SPIGOT_ALL.format(version)
+fun DependencyHandler.spigotAll(version: String = SPIGOT_ALL.defaultVersion) = SPIGOT_ALL.format(version)
 
-fun DependencyHandler.minecraftServer(version: String = "") = MINECRAFT_SERVER.format(version)
+fun DependencyHandler.bungeecord(version: String = BUNGEECORD.defaultVersion) = BUNGEECORD.format(version)
 
-fun DependencyHandler.paper(version: String = "") = PAPER.format(version)
+fun DependencyHandler.minecraftServer(version: String = MINECRAFT_SERVER.defaultVersion) = MINECRAFT_SERVER.format(version)
 
-fun DependencyHandler.bukkit(version: String = "") = BUKKIT.format(version)
+fun DependencyHandler.paper(version: String = PAPER.defaultVersion) = PAPER.format(version)
 
-fun DependencyHandler.craftbukkit(version: String = "") = CRAFT_BUKKIT.format(version)
+fun DependencyHandler.bukkit(version: String = BUKKIT.defaultVersion) = BUKKIT.format(version)
 
-fun DependencyHandler.protocolLib(version: String = "") = PROTOCOL_LIB.format(version)
+fun DependencyHandler.craftbukkit(version: String = CRAFT_BUKKIT.defaultVersion) = CRAFT_BUKKIT.format(version)
 
-fun DependencyHandler.vault(version: String = "") = VAULT.format(version)
+fun DependencyHandler.protocolLib(version: String = PROTOCOL_LIB.defaultVersion) = PROTOCOL_LIB.format(version)
 
-fun DependencyHandler.luckPerms(version: String = "") = LUCK_PERMS.format(version)
+fun DependencyHandler.vault(version: String = VAULT.defaultVersion) = VAULT.format(version)
 
-fun DependencyHandler.worldedit(version: String = "") = WORLD_EDIT.format(version)
+fun DependencyHandler.luckPerms(version: String = LUCK_PERMS.defaultVersion) = LUCK_PERMS.format(version)
 
-fun DependencyHandler.worldguard(version: String = "") = WORLD_GUARD.format(version)
+fun DependencyHandler.worldedit(version: String = WORLD_EDIT.defaultVersion) = WORLD_EDIT.format(version)
 
-fun DependencyHandler.commandhelper(version: String = "") = COMMAND_HELPER.format(version)
+fun DependencyHandler.worldguard(version: String = WORLD_GUARD.defaultVersion) = WORLD_GUARD.format(version)
 
-fun DependencyHandler.bStats(version: String = "") = BSTATS.format(version)
+fun DependencyHandler.commandhelper(version: String = COMMAND_HELPER.defaultVersion) = COMMAND_HELPER.format(version)
 
-fun DependencyHandler.bStatsLite(version: String = "") = BSTATS_LITE.format(version)
+fun DependencyHandler.bStats(version: String = BSTATS.defaultVersion) = BSTATS.format(version)
 
-fun DependencyHandler.lombok(version: String = "") = LOMBOK.format(version)
+fun DependencyHandler.bStatsLite(version: String = BSTATS_LITE.defaultVersion) = BSTATS_LITE.format(version)
+
+fun DependencyHandler.lombok(version: String = LOMBOK.defaultVersion) = LOMBOK.format(version)
