839ed422784ff339fcaa93c2abfb021250ce16a1
EntryPointKR <entrypointkr@gmail.com>
2020-03-31 22:02:29 +0900

    Security update: gradle-plugin-publish 0.10.1 to 0.11.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("groovy")
     id("org.jetbrains.kotlin.jvm") version "1.3.70"
     id("com.github.johnrengelman.shadow") version "5.2.0"
-    id("com.gradle.plugin-publish") version "0.10.1" apply false
+    id("com.gradle.plugin-publish") version "0.11.0" apply false
     id("com.jfrog.bintray") version "1.8.4" apply false
 }
 
