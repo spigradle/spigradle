ccdaf425c4747881167bf21ff32f43ea0e367366
scpketer <scpketer@protonmail.ch>
2020-03-31 08:43:41 +0400

    Essentials X latest version updated

--- src/main/kotlin/kr/entree/spigradle/kotlin/RepositoryHandlers.kt
+++ src/main/kotlin/kr/entree/spigradle/kotlin/RepositoryHandlers.kt
@@ -33,4 +33,6 @@ fun RepositoryHandler.codemc(configure: MavenArtifactRepository.() -> Unit = {})
 
 fun RepositoryHandler.enderZone(configure: MavenArtifactRepository.() -> Unit = {}) = maven(Repositories.ENDER_ZONE, configure)
 
+fun RepositoryHandler.essentialsX(configure: MavenArtifactRepository.() -> Unit = {}) = enderZone(configure)
+
 fun RepositoryHandler.bStats(configure: MavenArtifactRepository.() -> Unit = {}) = codemc(configure)
