4a4ec901d831e89fd70c6aaf9d7e947aa6ac3f8d
Junhyung Im <entrypointkr@gmail.com>
2020-05-20 13:23:10 +0900

    Fix for passing tests

--- src/main/kotlin/kr/entree/spigradle/module/spigot/SpigotPlugin.kt
+++ src/main/kotlin/kr/entree/spigradle/module/spigot/SpigotPlugin.kt
@@ -1,83 +1,85 @@
-@file:Suppress("UnstableApiUsage")
-
 package kr.entree.spigradle.module.spigot
 
 import groovy.lang.Closure
 import kr.entree.spigradle.data.Dependencies
 import kr.entree.spigradle.data.Dependency
-import kr.entree.spigradle.internal.*
-import kr.entree.spigradle.kotlin.maven
+import kr.entree.spigradle.internal.Groovies
+import kr.entree.spigradle.internal.Messages
+import kr.entree.spigradle.internal.PLUGIN_APT_DEFAULT_PATH
+import kr.entree.spigradle.internal.toFieldEntries
 import kr.entree.spigradle.module.common.SpigradlePlugin
 import kr.entree.spigradle.module.common.task.GenerateYamlTask
 import kr.entree.spigradle.module.spigot.data.SpigotDependencies
 import kr.entree.spigradle.module.spigot.data.SpigotRepositories
-import kr.entree.spigradle.module.spigot.data.setLoadGroovyExtension
+import kr.entree.spigradle.module.spigot.data.setSpigotExtension
 import kr.entree.spigradle.module.spigot.extension.SpigotPluginDescription
-import org.gradle.api.GradleException
+import notNull
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.internal.artifacts.dsl.DefaultRepositoryHandler.BINTRAY_JCENTER_URL
-import org.gradle.api.plugins.ExtraPropertiesExtension
+import org.gradle.api.tasks.compile.AbstractCompile
+import org.gradle.kotlin.dsl.*
 import org.gradle.language.jvm.tasks.ProcessResources
 import java.io.File
 
 /**
  * Created by JunHyung Lim on 2020-04-28
  */
+@Suppress("UnstableApiUsage")
 class SpigotPlugin : Plugin<Project> { // TODO: Shortcuts, Plugin YAML Generation, Main class auto detection
     companion object {
-        const val YAML_GEN_TASK_ID = "spigotPluginYaml"
+        const val YAML_GEN_TASK_NAME = "spigotPluginYaml"
+        const val EXTENSION_NAME = "spigot"
     }
 
     override fun apply(project: Project) {
         with(project) {
-            pluginManager.apply<SpigradlePlugin>()
+            pluginManager.apply(SpigradlePlugin::class)
             setupYamlGenTask()
             setupGroovyExtensions()
         }
     }
 
     private fun Project.setupYamlGenTask() {
-        val description = extensions.create<SpigotPluginDescription>("spigot", this)
-        val generateTask = tasks.create<GenerateYamlTask>(YAML_GEN_TASK_ID) {
+        val description = extensions.create<SpigotPluginDescription>(EXTENSION_NAME, this)
+        val generateTask = tasks.create(YAML_GEN_TASK_NAME, GenerateYamlTask::class) {
             doFirst {
-                validateDescription(description, this@setupYamlGenTask)
-            }
-            afterEvaluate {
-                setOptionsToMap(description)
+                description.setDefaults(this@setupYamlGenTask)
+                validateDescription(description)
+                setToOptionMap(description)
             }
         }
-        Groovies.getExtensionFrom(description).setLoadGroovyExtension()
-        tasks.findByBoth<ProcessResources>("processResources") {
-            from(generateTask.temporaryDir)
-            finalizedBy(generateTask)
-        }
+        Groovies.getExtensionFrom(description).setSpigotExtension()
+        val processResources: ProcessResources by tasks
+        processResources.from(generateTask)
     }
 
-    private fun validateDescription(description: SpigotPluginDescription, project: Project) {
-        description.apply {
-            main = main ?: project.getSpigotPluginMainOrThrow()
-            name = name ?: project.name
-            version = version ?: project.version.toString()
-        }
+    private fun SpigotPluginDescription.setDefaults(project: Project) {
+        main = main ?: project.spigotPluginMain
+        name = name ?: project.name
+        version = version ?: project.version.toString()
     }
 
-    private fun Project.getSpigotPluginMainOrThrow(): String? {
-        val found = runCatching { // get APT result or find out
-            File(project.buildDir, PLUGIN_APT_DEFAULT_PATH).readText()
+    private fun validateDescription(description: SpigotPluginDescription) {
+        notNull(description.main) { Messages.noMainFound(EXTENSION_NAME, YAML_GEN_TASK_NAME) }
+    }
+
+    private val Project.spigotPluginMain: String?
+        get() = runCatching { // get APT result or find out
+            val file = File(project.buildDir, PLUGIN_APT_DEFAULT_PATH)
+            file.readText()
         }.getOrNull() ?: findSpigotPluginMain()
-        return found ?: throw GradleException(Messages.noMainFound("spigot", YAML_GEN_TASK_ID))
-    }
 
-    private fun findSpigotPluginMain(): String? {
-        TODO()
+    private fun Project.findSpigotPluginMain(): String? {
+        tasks.withType<AbstractCompile>().map { it.destinationDir }.forEach {
+            println(it.absolutePath) // TODO
+        }
+        return null
     }
 
     private fun Project.setupGroovyExtensions() {
-        extensions.findByBoth<ExtraPropertiesExtension>("ext")?.apply {
-            setupDependencies()
-            setupRepositories()
-        }
+        setupRepositories()
+        setupDependencies()
     }
 
     private fun Project.setupRepositories() {
