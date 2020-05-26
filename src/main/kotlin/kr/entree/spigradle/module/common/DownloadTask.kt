bc673db7fdd53dc3996530b1d4d70424e8655d02
EntryPointKR <entrypointkr@gmail.com>
2020-05-27 00:22:48 +0900

    Implement prototype of build&run tasks

---
+++ src/main/kotlin/kr/entree/spigradle/module/common/DownloadTask.kt
@@ -0,0 +1,27 @@
+package kr.entree.spigradle.module.common
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.SkipWhenEmpty
+import org.gradle.api.tasks.TaskAction
+import java.io.File
+import java.net.URL
+
+/**
+ * Created by JunHyung Lim on 2020-05-26
+ */
+open class DownloadTask : DefaultTask() {
+    @get:Input
+    @SkipWhenEmpty
+    lateinit var source: String
+
+    @get:OutputFile
+    @SkipWhenEmpty
+    lateinit var destination: File
+
+    @TaskAction
+    fun download() = destination.apply {
+        parentFile.mkdirs()
+    }.writeBytes(URL(source).readBytes())
+}
