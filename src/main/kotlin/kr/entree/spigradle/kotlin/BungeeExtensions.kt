464f7f4374ce2abcb0b9db3a0f4ea2e9e71f339b
JunHyung Lim <entrypointkr@gmail.com>
2020-06-20 13:25:15 +0900

    Add copyright on all files header, kdocs for extensions
    Signed-off-by: JunHyung Lim <entrypointkr@gmail.com>

--- src/main/kotlin/kr/entree/spigradle/kotlin/BungeeExtensions.kt
+++ src/main/kotlin/kr/entree/spigradle/kotlin/BungeeExtensions.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020 Spigradle contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package kr.entree.spigradle.kotlin
 
 import kr.entree.spigradle.data.BungeeDependencies
@@ -6,11 +22,11 @@ import org.gradle.api.artifacts.dsl.RepositoryHandler
 import org.gradle.api.artifacts.repositories.MavenArtifactRepository
 
 /**
- * Repositories
+ * The repo shortcut for Sonatype same as sonatype(), related with Bungeecord.
  */
 fun RepositoryHandler.bungeecord(configure: MavenArtifactRepository.() -> Unit = {}) = sonatype(configure)
 
 /**
- * Dependencies
+ * The dependency shortcut for Bungeecord, requires repository sonatype() or bungeecord().
  */
 fun DependencyHandler.bungeecord(version: String? = null) = BungeeDependencies.BUNGEE_CORD.format(version)
--- src/main/kotlin/kr/entree/spigradle/module/common/SpigradlePlugin.kt
+++ src/main/kotlin/kr/entree/spigradle/module/common/SpigradlePlugin.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020 Spigradle contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package kr.entree.spigradle.module.common
 
 import groovy.lang.Closure
@@ -7,9 +23,11 @@ import kr.entree.spigradle.internal.Groovies
 import kr.entree.spigradle.internal.PLUGIN_APT_DEFAULT_PATH
 import kr.entree.spigradle.internal.PLUGIN_APT_RESULT_PATH_KEY
 import kr.entree.spigradle.internal.toFieldEntries
+import kr.entree.spigradle.kotlin.mockBukkit
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.api.internal.artifacts.dsl.DefaultRepositoryHandler.BINTRAY_JCENTER_URL
 import org.gradle.api.invocation.Gradle
 import org.gradle.api.plugins.JavaPlugin
@@ -20,6 +38,8 @@ import org.gradle.plugins.ide.idea.IdeaPlugin
 import org.gradle.plugins.ide.idea.model.IdeaModel
 import org.jetbrains.gradle.ext.IdeaExtPlugin
 import java.io.File
+import kotlin.reflect.full.declaredMemberExtensionFunctions
+import kotlin.reflect.full.memberExtensionFunctions
 
 /**
  * Created by JunHyung Lim on 2020-05-18
@@ -109,6 +129,10 @@ class SpigradlePlugin : Plugin<Project> {
                 fun doCall(version: String?) = dependency.format(version)
             })
         }
+        ext.set("mockBukkit", object: Closure<Any>(this, this) {
+            fun doCall(spigotVersion: String? = null, mockBukkitVersion: String? = null) =
+                    dependencies.mockBukkit(spigotVersion, mockBukkitVersion)
+        }) // Can be replaced by reflection to SpigotExtensionsKt
     }
 
     private fun Project.setupAnnotationProcessorOptions() {
