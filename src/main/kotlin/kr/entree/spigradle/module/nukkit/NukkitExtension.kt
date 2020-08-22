e5b5d8d45ba46bf3d7d9ab319d515e68273899fe
Junhyung Im <entrypointkr@gmail.com>
2020-08-23 07:28:38 +0900

    feat: set default the `description` to `project.description`
    Signed-off-by: EntryPointKR <entrypointkr@gmail.com>

--- src/main/kotlin/kr/entree/spigradle/module/nukkit/NukkitExtension.kt
+++ src/main/kotlin/kr/entree/spigradle/module/nukkit/NukkitExtension.kt
@@ -111,7 +111,7 @@ open class NukkitExtension(project: Project) : StandardDescription {
     override var main: String? = null
     override var name: String? = null
     override var version: String? = null
-    var description: String? = null
+    override var description: String? = null
     var website: String? = null
     var authors: List<String> = emptyList()
     var api: List<String> = emptyList()
