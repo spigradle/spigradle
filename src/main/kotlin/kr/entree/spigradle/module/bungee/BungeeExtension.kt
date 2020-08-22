0dbf677104e94f8f9993665bcc7a5caa80eae70a
EntryPointKR <entrypointkr@gmail.com>
2020-08-23 08:57:48 +0900

    fix: compile error
    Signed-off-by: EntryPointKR <entrypointkr@gmail.com>

--- src/main/kotlin/kr/entree/spigradle/module/bungee/BungeeExtension.kt
+++ src/main/kotlin/kr/entree/spigradle/module/bungee/BungeeExtension.kt
@@ -58,7 +58,7 @@ open class BungeeExtension(project: Project) : StandardDescription {
     override var main: String? = null
     override var name: String? = null
     override var version: String? = null
-    var description: String? = null
+    override var description: String? = null
     var author: String? = null
     var depends: List<String> = emptyList()
     var softDepends: List<String> = emptyList()
