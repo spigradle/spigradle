975d289eae8d66f146ba5b1fb2c2b436d132bdee
EntryPointKR <entrypointkr@gmail.com>
2020-09-03 16:36:27 +0900

    fix(debug-run): also find in runtimeClasspath and bigger one for prepare plugins
    Signed-off-by: EntryPointKR <entrypointkr@gmail.com>

--- src/main/kotlin/kr/entree/spigradle/internal/Messages.kt
+++ src/main/kotlin/kr/entree/spigradle/internal/Messages.kt
@@ -26,4 +26,5 @@ internal object Messages {
         or set the 'main' property in $extensionName {} block on build.gradle,
         or just disable $taskName task: 'tasks.$taskName.enabled = false'
     """.trimIndent()
+    const val UNRESOLVED_DEPENDENCIES: String = "Unable to resolve the plugin dependencies"
 }
